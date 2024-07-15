355e003af0bc0901ceb1e52785aea59e4b020265
Dennis <dennisjean0226@gmail.com>
2024-07-15 13:00:48 +0800

    mashalla

---
+++ al.js
@@ -0,0 +1,33 @@
+console.log("i AM GONn  make a purple diagram ");
+console.log("make a flappy bird")
+
+const readline = require('readline');
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+r1.question("what is your name? ", function(name) {
+    console.log(`Hello ${name}!`);
+
+if(name ==='c')
+{
+    console.log("ypou r  nob");
+}
+
+
+for(let i = 0; i< 12;i++)
+{
+    console.log("i am going to make a purple diagram");
+}
+    
+
+
+
+    r1.close();
+});
+
+const person = {name1:"nig",nam2 :"name2?"}
+console.log(person.name1);
+
+
---
+++ shortestJobFirstAlgo.js
@@ -0,0 +1,5 @@
+const  temp = 2212
+var prompt = require("prompt");
+prompt.start();
+        prompt.get(["name"],function(err,res){     "hello".concat(res.name,"how old r you ")}); 
+        console.log("enter your name mf")            
